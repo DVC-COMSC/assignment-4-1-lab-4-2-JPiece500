1a813eed6d0d81708cd33b8c3a3afc8ee3c18b9e
theJPiece <112590721+JPiece500@users.noreply.github.com>
2022-10-11 17:43:14 +0000

    j

--- main.cpp
+++ main.cpp
@@ -10,50 +10,44 @@
 //*  computes the total cost of the purchase
 //** Input Validation : Input must be a positive integer
 //**    If we have negative input, just quit the program
-#include <iomanip>
 #include <iostream>
-using namespace std;
-
-int main() {
 #include <iomanip>
-#include <iostream>
 using namespace std;
-
 int main() {
-  const double PERC19 = 0.2;
-  const double PERC49 = 0.3;
-  const double PERC99 = 0.4;
-  const double PERC100 = 0.5;
-  const double Price = 99.00;
-  double totalCost, originalAmount, discountAmount, dRate;
-  int numberofSold;
+  double perc1= .2, perc2 = .3, perc3 = .4, perc4 = .5;
+  
+  double price = 99.00;
+  
+  double total, origam, discam, drate;
+  int numsold;
 
-  cout << "Enter the number of units sold\n";
-  cin >> numberofSold;
+cout << "Units sold?: ";
+  cin >> numsold;
 
-  // Input Validation
-  if (numberofSold < 0) {
-    cout << "Input should be a positive number\n";
-    exit(0);
+if (numsold < 0) {
+  cout << "Enter a positive number.";
   }
-  if (numberofSold < 10)
-    dRate = 1.0;
-  else if (numberofSold < 20)
-    dRate = PERC19;
-  else if (numberofSold < 50)
-    dRate = PERC49;
-  else if (numberofSold < 100)
-    dRate = PERC99;
-  else {
-    dRate = PERC100;
-    }
-    
-  originalAmount = numberofSold * Price;
-  discountAmount = originalAmount * dRate;
-  totalCost = originalAmount - discountAmount;
-
-  cout << setprecision(2) << fixed;
-  cout << "Original amount is " << originalAmount << endl;
-  cout << "Discount amount is " << discountAmount << endl;
-  cout << "Total Price is " << totalCost << endl;
+if (numsold < 10)
+  drate = 1.0;
+else if (numsold < 20)
+  drate = perc1;
+else if (numsold < 50)
+  drate = perc2;
+else if (numsold < 100)
+  drate = perc3;
+else {
+  drate = perc4;
   }
+  
+origam = numsold * price;
+discam = origam * drate; 
+total = origam - discam; 
+
+cout << setprecision(2) << fixed;
+cout << "Original amount is " << origam << endl;
+cout << "Discount amount is " << discam << endl;
+cout << "Total price is " << total << endl;
+
+  
+  
+}
